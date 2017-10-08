eadd96a7ade759ae109c85ad60aa063c6e85f30b
Antoine Carossio <antoine.carossio@me.com>
2017-10-08 11:59:17 +0200

    Changement de styles des Espaces X et Pro

--- entreprises.php
+++ entreprises.php
@@ -6,7 +6,7 @@
 -->
 <html>
 	<head>
-		<title>Binet Réseau - Espace Pro</title>
+		<title>Binet Réseau - Espace X</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
@@ -27,81 +27,50 @@
 							<h2>Espace Pro</h2>
 							<p>Destiné aux entreprises et aux associations</p>
 						</header>
-						<div class="row 150%">
-							<div class="4u 12u$(medium)">
 
-								<!-- Sidebar -->
-									<section id="sidebar">
-										<section>
-											<h3>Pourquoi nouer un partenariat ?</h3>
-											<p>Description…</p>
-											<footer>
-												<ul class="actions">
-													<li><a href="#" class="button">Learn More</a></li>
-												</ul>
-											</footer>
-										</section>
-										<hr />
-										<section>
-											<a href="#" class="image fit"><img src="images/pic06.jpg" alt="" /></a>
-											<h3>Amet Lorem Tempus</h3>
-											<p>Sed tristique purus vitae volutpat commodo suscipit amet sed nibh. Proin a ullamcorper sed blandit. Sed tristique purus vitae volutpat commodo suscipit ullamcorper sed blandit lorem ipsum dolore.</p>
-											<footer>
-												<ul class="actions">
-													<li><a href="#" class="button">Learn More</a></li>
-												</ul>
-											</footer>
-										</section>
-									</section>
+						<!-- Content -->
+							<section id="content">
+								<a href="#" class="image no-background fit"><img src="images/pic05.jpg" alt="" /></a>
+								<h3>Nous contacter</h3>
 
-							</div>
-							<div class="8u$ 12u$(medium) important(medium)">
+								<form method="post" action="#">
+									<div class="row uniform 50%">
+										<div class="6u 12u$(xsmall)">
+											<input type="text" name="nom" id="nom" value="" placeholder="Nom" />
+										</div>
+										<div class="6u 12u$(xsmall)">
+											<input type="text" name="prenom" id="prenom" value="" placeholder="Prénom" />
+										</div>
+										<div class="6u 12u$(xsmall)">
+											<input type="text" name="organisation" id="organisation" value="" placeholder="Nom de l'organisation" />
+										</div>
+										<div class="6u$ 12u$(xsmall)">
+											<input type="email" name="email" id="email" value="" placeholder="Email professionnel" />
+										</div>
 
-								<!-- Content -->
-									<section id="content">
-										<a href="#" class="image no-background fit"><img src="images/pic05.jpg" alt="" /></a>
-										<h3>Nous contacter</h3>
+										<div class="12u$">
+											<input type="email" name="email" id="email" value="" placeholder="Objet" />
+										</div>
 
-										<form method="post" action="#">
-											<div class="row uniform 50%">
-												<div class="6u 12u$(xsmall)">
-													<input type="text" name="nom" id="nom" value="" placeholder="Nom" />
-												</div>
-												<div class="6u 12u$(xsmall)">
-													<input type="text" name="prenom" id="prenom" value="" placeholder="Prénom" />
-												</div>
-												<div class="6u 12u$(xsmall)">
-													<input type="text" name="organisation" id="organisation" value="" placeholder="Nom de l'organisation" />
-												</div>
-												<div class="6u$ 12u$(xsmall)">
-													<input type="email" name="email" id="email" value="" placeholder="Email professionnel" />
-												</div>
+										<div class="12u$">
+											<textarea name="message" id="message" placeholder="Votre message" rows="6"></textarea>
+										</div>
+										<div class="12u$">
+											<ul class="actions">
+												<li><input type="submit" value="Envoyer" class="special" /></li>
+												<li><input type="reset" value="Reset" /></li>
+											</ul>
+										</div>
 
-												<div class="12u$">
-													<input type="email" name="email" id="email" value="" placeholder="Objet" />
-												</div>
+										<div class="6u 12u$(medium)">
+											<input type="checkbox" id="copy" name="copy">
+											<label for="copy">M'envoyer une copie de ce message</label>
+										</div>
+									</div>
+								</form>
 
-												<div class="12u$">
-													<textarea name="message" id="message" placeholder="Votre message" rows="6"></textarea>
-												</div>
-												<div class="12u$">
-													<ul class="actions">
-														<li><input type="submit" value="Envoyer" class="special" /></li>
-														<li><input type="reset" value="Reset" /></li>
-													</ul>
-												</div>
+							</section>
 
-												<div class="6u 12u$(medium)">
-													<input type="checkbox" id="copy" name="copy">
-													<label for="copy">M'envoyer une copie de ce message</label>
-												</div>
-											</div>
-										</form>
-
-									</section>
-
-							</div>
-						</div>
 					</div>
 				</div>
 
--- etudiants.php
+++ etudiants.php
@@ -6,7 +6,7 @@
 -->
 <html>
 	<head>
-		<title>Binet Réseau - Espace X</title>
+		<title>Right Sidebar - Landed by HTML5 UP</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
@@ -18,7 +18,7 @@
 		<div id="page-wrapper">
 
 			<!-- Header -->
-			<?php include('header.php'); ?>
+				<?php include('header.php'); ?>
 
 			<!-- Main -->
 				<div id="main" class="wrapper style1">
@@ -27,47 +27,91 @@
 							<h2>Espace X</h2>
 							<p>Liens utiles à la vie sur le platâl</p>
 						</header>
+						<div class="row 150%">
+							<div class="8u 12u$(medium)">
 
-						<!-- Content -->
-							<section id="content">
-								<a href="#" class="image no-background fit"><img src="images/pic07.jpg" alt="" /></a>
-								<h2>WikiBR</h2>
-								    <p>Pour accéder au WikiBR, c'est sur : <a href="https://wikibr.binets.fr">wikibr.binets.fr</a>.</p>
-								    <h2>InfoBR</h2>
-								    <p>L'infoBR en ligne est ici : <a href=https://portail.polytechnique.edu/dsi/internet>http://infobr/</a></p>
-								    <h2>Contact</h2>
-								    <ul>
-								    <li>Pour tout problème technique : <a href="https://panix.binets.fr">PaniX</a>;</li>
-								    <li>pour ce qui concerne l'hébergement d'un site pour un binet, c'est : <a href="mailto:hebergement@eleves.polytechnique.fr">hebergement@eleves.polytechnique.fr</a> ;</li>
-								    <li>pour toute question concernant Frankiz, c'est : <a href="mailto:web@efrankiz.net">web@frankiz.net</a> ;</li>
-								    <li>pour contacter le bureau : <a href="mailto:bureau@eleves.polytechnique.fr">bureau@eleves.polytechnique.fr</a> ;</li>
-								    <li>enfin, pour écrire au BR tout entier :  <a href="mailto:br@eleves.polytechnique.fr">br@eleves.polytechnique.fr</a>.</li>
-								    </ul>
+								<!-- Content -->
+									<section id="content">
+									<section id="content">
+										<a href="#" class="image no-background fit"><img src="images/pic07.jpg" alt="" /></a>
+										<h2>WikiBR</h2>
+										    <p>Pour accéder au WikiBR, c'est sur : <a href="https://wikibr.binets.fr">wikibr.binets.fr</a>.</p>
+										    <h2>InfoBR</h2>
+										    <p>L'infoBR en ligne est ici : <a href=https://portail.polytechnique.edu/dsi/internet>http://infobr/</a></p>
+										    <h2>Contact</h2>
+										    <ul>
+										    <li>Pour tout problème technique : <a href="https://panix.binets.fr">PaniX</a>;</li>
+										    <li>pour ce qui concerne l'hébergement d'un site pour un binet, c'est : <a href="mailto:hebergement@eleves.polytechnique.fr">hebergement@eleves.polytechnique.fr</a> ;</li>
+										    <li>pour toute question concernant Frankiz, c'est : <a href="mailto:web@efrankiz.net">web@frankiz.net</a> ;</li>
+										    <li>pour contacter le bureau : <a href="mailto:bureau@eleves.polytechnique.fr">bureau@eleves.polytechnique.fr</a> ;</li>
+										    <li>enfin, pour écrire au BR tout entier :  <a href="mailto:br@eleves.polytechnique.fr">br@eleves.polytechnique.fr</a>.</li>
+										    </ul>
 
-								    <h2>Support</h2>
-								    <h3>IRC</h3>
-								    <p>Pour obtenir sur support en live, tu peux nous trouver sur <a href="https://wikix.polytechnique.org/IRC">IRC</a> :</p>
-								    <ul>
-								        <li>serveur : <code>irc</code> ou <code>ircserver</code> :
-								        	<ul>
-									         	<li><code>irc.eleves.polytechnique.fr/ircserver.eleves.polytechnique.fr</code> sur le platâl;</li>
-									         	<li><code>irc.rezosup.org</code> à l'extérieur.</li>
-								         	</ul>
-								         </li>
-								        <li>port 6667 : connexion non sécurisée</li>
-								        <li>port 6767 : connexion SSL</li>
-								        <li>channel : <code>#br-support</code></li>
-								    </ul>
+										    <h2>Support</h2>
+										    <h3>IRC</h3>
+										    <p>Pour obtenir sur support en live, tu peux nous trouver sur <a href="https://wikix.polytechnique.org/IRC">IRC</a> :</p>
+										    <ul>
+										        <li>serveur : <code>irc</code> ou <code>ircserver</code> :
+										        	<ul>
+											         	<li><code>irc.eleves.polytechnique.fr/ircserver.eleves.polytechnique.fr</code> sur le platâl;</li>
+											         	<li><code>irc.rezosup.org</code> à l'extérieur.</li>
+										         	</ul>
+										         </li>
+										        <li>port 6667 : connexion non sécurisée</li>
+										        <li>port 6767 : connexion SSL</li>
+										        <li>channel : <code>#br-support</code></li>
+										    </ul>
 
-								    <h3>Titus</h3>
-								    <p>Tu peux également nous contacter en utilisant le chat promo : <a href="https://titus.binets.fr/">Titus</a></p>
-							</section>
+										    <h3>Titus</h3>
+										    <p>Tu peux également nous contacter en utilisant le chat promo : <a href="https://titus.binets.fr/">Titus</a></p>
+									</section>
 
+							</div>
+							<div class="4u$ 12u$(medium)">
+
+								<!-- Sidebar -->
+									<section id="sidebar">
+										<section>
+											<h3>Magna Feugiat</h3>
+											<p>Sed tristique purus vitae volutpat commodo suscipit amet sed nibh. Proin a ullamcorper sed blandit. Sed tristique purus vitae volutpat commodo suscipit ullamcorper commodo suscipit amet sed nibh. Proin a ullamcorper sed blandit..</p>
+											<footer>
+												<ul class="actions">
+													<li><a href="#" class="button">Learn More</a></li>
+												</ul>
+											</footer>
+										</section>
+										<hr />
+										<section>
+											<a href="#" class="image fit"><img src="images/pic07.jpg" alt="" /></a>
+											<h3>Amet Lorem Tempus</h3>
+											<p>Sed tristique purus vitae volutpat commodo suscipit amet sed nibh. Proin a ullamcorper sed blandit. Sed tristique purus vitae volutpat commodo suscipit ullamcorper sed blandit lorem ipsum dolore.</p>
+											<footer>
+												<ul class="actions">
+													<li><a href="#" class="button">Learn More</a></li>
+												</ul>
+											</footer>
+										</section>
+									</section>
+
+							</div>
+						</div>
 					</div>
 				</div>
 
 			<!-- Footer -->
-			<?php include('footer.php'); ?>
+				<footer id="footer">
+					<ul class="icons">
+						<li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="#" class="icon alt fa-linkedin"><span class="label">LinkedIn</span></a></li>
+						<li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="#" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
+						<li><a href="#" class="icon alt fa-envelope"><span class="label">Email</span></a></li>
+					</ul>
+					<ul class="copyright">
+						<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
+				</footer>
 
 		</div>
 
