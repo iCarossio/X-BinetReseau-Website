13cfd8900b3d6fdd4c378f82850ebd2eff3ac0be
Antoine Carossio <antoine.carossio@me.com>
2017-11-10 17:08:36 +0100

    Suppression de l'adresse mail dans le footer

--- footer.php
+++ footer.php
@@ -7,7 +7,6 @@
 		-->
 		<li><a href="https://www.linkedin.com/company/11089000/" target="_blank" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
 		<li><a href="https://github.com/BinetReseau" target="_blank" class="icon fa-github"><span class="label">GitHub</span></a></li>
-		<li><a href="mailto:br@eleves.polytechnique.fr" class="icon fa-envelope"><span class="label">Email</span></a></li>
 	</ul>
 	<ul class="copyright">
 		<li>&copy; 2017 Binet Réseau.</li>
